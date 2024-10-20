b061f2d4f3696d09464b4deb65aa62b93955d55b
Mathew <matheoshanna10@gmail.com>
2024-10-20 15:57:40 -0300

    feat: modelando temporadas

--- src/main/java/br/com/mathew/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/mathew/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.mathew.screenmatch;
 
 import br.com.mathew.screenmatch.model.DadosEpisodio;
 import br.com.mathew.screenmatch.model.DadosSerie;
+import br.com.mathew.screenmatch.model.DadosTemporada;
 import br.com.mathew.screenmatch.service.ConsumoApi;
 import br.com.mathew.screenmatch.service.CoverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -32,5 +36,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
 
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i=1; i<=dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=c142b0d2");
+
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
